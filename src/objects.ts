3bf8e703a6f64d771e3dd658f1c15fc19e909bed
Jesse Russell <jesserussell411@gmail.com>
2023-09-07 20:29:07 -0600

    removed getownentries

--- src/objects.ts
+++ src/objects.ts
@@ -15,24 +15,6 @@ export function getOwnPropertyKeys<K extends keyof any>(
 }
 
 /**
- * @returns The object's own entries.
- */
-export function getOwnEntries<K extends keyof any, V>(
-    object: Record<K, any>
-): [K & (string | symbol), V][] {
-    return [
-        ...Object.getOwnPropertyNames(object).map((n) => [
-            n,
-            (object as any)[n],
-        ]),
-        ...Object.getOwnPropertySymbols(object).map((s) => [
-            s,
-            (object as any)[s],
-        ]),
-    ] as any;
-}
-
-/**
  * Runtime equivalent to {@link Pick}.
  * @param fields The fields to include.
  * @return A shallow copy of the object with only the fields specified.
